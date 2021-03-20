985da433d9a47b0f07e822f0de5b611f677b5f54
zvnico <nicolas.baconnier@efrei.net>
2021-03-20 22:37:34 +0100

    add Automate.py, automate.txt, constantes.py

---
+++ Automate.py
@@ -0,0 +1,7 @@
+class Automate:
+    def __init__(self, nom_fichier):
+        self.lire_automate_sur_fichier(nom_fichier)
+
+    def lire_automate_sur_fichier(self, nom_fichier):
+        with open(nom_fichier.txt) as automatetxt:
+            pass
