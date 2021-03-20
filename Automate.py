1aff8728a92aefd8962d065fd7661d8b165bc957
zvnico <nicolas.baconnier@efrei.net>
2021-03-20 23:12:59 +0100

    tk file dialog

--- Automate.py
+++ Automate.py
@@ -1,7 +1,17 @@
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+
 class Automate:
-    def __init__(self, nom_fichier):
-        self.lire_automate_sur_fichier(nom_fichier)
+    def __init__(self):
+        filename = askopenfilename(initialdir="automates/", filetypes=[('Text Files', '*.txt')])
+        self.lire_automate_sur_fichier(filename)
 
     def lire_automate_sur_fichier(self, nom_fichier):
-        with open(nom_fichier.txt) as automatetxt:
-            pass
+        """fonction pour restranscrit un automate depuis un fichier"""
+        with open(nom_fichier) as automatetxt:
+            for line in automatetxt.readlines():
+                print(line, end="")
+
+    def ecrire_automate_sur_fichier(self, nom_fichier):
+        """fonction pour restranscrit un automate dans un fichier depuis les attributs de cette classe"""
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from Automate import Automate
+
+if __name__ == '__main__':
+    automate = Automate()
